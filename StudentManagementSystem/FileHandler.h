be8113217bb7a1e8ed725cd302494eae3cc8a1e7
CodeingBoy <codeingboy@gmail.com>
2017-01-30 14:04:11 +0800

    introduced FileHandler

---
+++ StudentManagementSystem/FileHandler.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstdio>
+
+class CFileHandler
+{
+public:
+    CFileHandler(const wchar_t* filename, bool bRead);
+    ~CFileHandler();
+    bool ReadLine(wchar_t* output);
+    wchar_t* ReadLine();
+    bool Write(wchar_t* content);
+    bool WriteLine(wchar_t* content);
+    int GetError();
+    bool isEOF();
+private:
+    bool bRead = true; // 读取还是保存
+    int err;
+    FILE* fp;
+};
+
